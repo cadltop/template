89f52e31c20d7ae9fd77f291970cad8377642b75
cadltop <158861368+cadltop@users.noreply.github.com>
2024-08-22 16:09:10 -0500

    Set images and fonts assets

--- webpack.common.js
+++ webpack.common.js
@@ -3,6 +3,11 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
 
   plugins: [
     new HtmlWebpackPlugin({
@@ -18,13 +23,15 @@ module.exports = {
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
-      }
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
     ]
-  },
-
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
   },
 };
